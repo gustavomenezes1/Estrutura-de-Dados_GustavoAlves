e75d237b288d9ce58d4f01e2bc568f291eabd510
Gustavo Alves <73456661+gustavomenezes1@users.noreply.github.com>
2022-07-25 20:20:20 -0300

    Add files via upload

---
+++ LAB_10/Fila_Desperdicio/fila.h
@@ -0,0 +1,13 @@
+typedef struct fila *Fila;
+
+Fila cria_fila();
+int fila_vazia(Fila f);
+int fila_cheia(Fila f);
+int insere_final(Fila f, int elem);
+int remove_inicio(Fila f, int *elem);
+int le_final(Fila f, int *elem);
+int imprime_fila(Fila f);
+
+
+
+
